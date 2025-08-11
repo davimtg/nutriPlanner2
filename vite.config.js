9c661edd186c47a92159f87fb4ede11200c4de3d
Davitab <davimtg2012@gmail.com>
2025-08-11 17:29:16 -0300

    fix: Remove import incorreto do Tailwind no vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-    strictPort: true,
-    hmr: {
-      clientPort: 5173
-    }
-  }
+  plugins: [react()],
 })
+
